9ce0fd6e2815006b19133ef365ebf64ba89b759b
Jose Hernandez <jose.hernandeztorres@yahoo.com>
2018-11-30 20:27:32 -0700

    Initial files

---
+++ assets/js/init.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.6.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBE6MVHrrVbxtAZyNxtb4yDF-Qgt3plGTk",
+    authDomain: "bc-train-schedule-94fbf.firebaseapp.com",
+    databaseURL: "https://bc-train-schedule-94fbf.firebaseio.com",
+    projectId: "bc-train-schedule-94fbf",
+    storageBucket: "bc-train-schedule-94fbf.appspot.com",
+    messagingSenderId: "320099494537"
+  };
+  firebase.initializeApp(config);
+</script>
